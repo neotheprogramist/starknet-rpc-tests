aceb9762ccf92a3b644b856aed35f5708030f745
Piotr Stec <piotr.stec2g@gmail.com>
2024-08-21 14:35:21 +0200

    invoke v3  signature verification

--- t9n/src/txn_validation/invoke.rs
+++ t9n/src/txn_validation/invoke.rs
@@ -1,6 +1,9 @@
-use super::constants::PREFIX_INVOKE;
+use std::error::Error;
+
+use super::constants::{DATA_AVAILABILITY_MODE_BITS, PREFIX_INVOKE};
 use starknet_types_core::curve::*;
 use starknet_types_core::felt::Felt;
+use starknet_types_core::hash::poseidon_hash_many;
 use starknet_types_rpc::v0_7_1::starknet_api_openrpc::*;
 use starknet_types_rpc::BroadcastedInvokeTxn;
 
@@ -41,8 +44,7 @@ fn verify_invoke_v1_signature(txn: &InvokeTxnV1<Felt>) -> Result<bool, VerifyErr
     let s_bytes = txn.signature[1];
 
     verify(&stark_key, &msg_hash, &r_bytes, &s_bytes)
-
-    }
+}
 
 fn verify_invoke_v3_signature(txn: &InvokeTxnV3<Felt>) -> Result<bool, VerifyError> {
     let chain_id = Felt::from_hex_unchecked("0x534e5f5345504f4c4941");
@@ -50,38 +52,124 @@ fn verify_invoke_v3_signature(txn: &InvokeTxnV3<Felt>) -> Result<bool, VerifyErr
     let stark_key = Felt::from_hex_unchecked(
         "0x39d9e6ce352ad4530a0ef5d5a18fd3303c3606a7fa6ac5b620020ad681cc33b",
     );
-    // Create the buffer for L1 resources and compute its hash
-    let mut resource_buffer_l1 = [
-        0, 0, b'L', b'1', b'_', b'G', b'A', b'S', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-    ];
-    // resource_buffer_l1[8..(8 + 8)]
-    //     .copy_from_slice(txn.resource_bounds.l1_gas.max_amount.as_bytes());
-    // resource_buffer_l1[(8 + 8)..]
-    //     .copy_from_slice(txn.resource_bounds.l1_gas.max_price_per_unit.as_bytes());
-    // let l1_gas_bounds = Felt::from_bytes_be(&resource_buffer_l1);
 
-    // Create the buffer for L2 resources and compute its hash
-    let resource_buffer_l2 = [
-        0, 0, b'L', b'2', b'_', b'G', b'A', b'S', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-    ];
-    let l2_gas_bounds = Felt::from_bytes_be(&resource_buffer_l2);
+    let msg_hash = calculate_invoke_v3_transaction_hash(&chain_id, &txn).unwrap();
 
-    let msg_hash = compute_hash_on_elements(&[
-        PREFIX_INVOKE,
-        Felt::THREE, // version
-        txn.nonce,
-        txn.sender_address,
-        // compute_hash_on_elements(&[Felt::ZERO, l1_gas_bounds, l2_gas_bounds]),
-        compute_hash_on_elements(&txn.paymaster_data),
-        chain_id,
-        //TODO :data_availability_modes,
-        compute_hash_on_elements(&txn.account_deployment_data),
-        compute_hash_on_elements(&txn.calldata),
-    ]);
     let r_bytes = txn.signature[0];
     let s_bytes = txn.signature[1];
 
     verify(&stark_key, &msg_hash, &r_bytes, &s_bytes)
+}
+
+fn calculate_invoke_v3_transaction_hash(
+    chain_id: &Felt,
+    txn: &InvokeTxnV3<Felt>,
+) -> Result<Felt, Box<dyn Error>> {
+    let common_fields = common_fields_for_hash(PREFIX_INVOKE, *chain_id, txn)?;
+    println!("common_fields {:?}", common_fields);
+    let account_deployment_data_hash = poseidon_hash_many(&txn.account_deployment_data);
+
+    let call_data_hash = poseidon_hash_many(&txn.calldata);
+
+    let fields_to_hash = [
+        common_fields.as_slice(),
+        &[account_deployment_data_hash],
+        &[call_data_hash],
+    ]
+    .concat();
+
+    let txn_hash = poseidon_hash_many(fields_to_hash.as_slice());
+    Ok(txn_hash)
+}
+
+/// Returns the array of Felts that reflects (tip, resource_bounds_for_fee) from SNIP-8
+fn get_resource_bounds_array(
+    txn: &InvokeTxnV3<Felt>,
+) -> Result<Vec<Felt>, Box<dyn Error>> {
+    let mut array = Vec::<Felt>::new();
+    array.push(txn.tip);
+
+    array.push(field_element_from_resource_bounds(
+        Resource::L1Gas,
+        &txn.resource_bounds.l1_gas,
+    )?);
+    array.push(field_element_from_resource_bounds(
+        Resource::L2Gas,
+        &txn.resource_bounds.l2_gas,
+    )?);
+    println!("{:?}", array);
+
+    Ok(array)
+}
+
+fn field_element_from_resource_bounds(
+    resource: Resource,
+    resource_bounds: &ResourceBounds,
+) -> Result<Felt, Box<dyn Error>> {
+    let resource_name_as_json_string = serde_json::to_value(resource)?;
+
+    // Ensure it's a string and get bytes
+    let resource_name_bytes = resource_name_as_json_string
+        .as_str()
+        .ok_or("Resource name is not a string")?
+        .as_bytes();
+    println!("0 {:?}", resource_name_bytes);
+
+    let max_amount_hex_str = resource_bounds.max_amount.as_str().trim_start_matches("0x");
+    let max_amount_u64 = u64::from_str_radix(max_amount_hex_str, 16)?;
+
+    let max_price_per_unit_hex_str = resource_bounds
+        .max_price_per_unit
+        .as_str()
+        .trim_start_matches("0x");
+    let max_price_per_unit_u64 = u128::from_str_radix(max_price_per_unit_hex_str, 16)?;
+
+    // (resource||max_amount||max_price_per_unit) from SNIP-8 https://github.com/starknet-io/SNIPs/blob/main/SNIPS/snip-8.md#protocol-changes
+    let bytes: Vec<u8> = [
+        resource_name_bytes,
+        max_amount_u64.to_be_bytes().as_slice(),
+        max_price_per_unit_u64.to_be_bytes().as_slice(),
+    ]
+    .into_iter()
+    .flatten()
+    .copied()
+    .collect();
+
+    Ok(Felt::from_bytes_be_slice(&bytes))
+}
+
+fn common_fields_for_hash(
+    tx_prefix: Felt,
+    chain_id: Felt,
+    txn: &InvokeTxnV3<Felt>,
+) -> Result<Vec<Felt>, Box<dyn Error>> {
+    let array: Vec<Felt> = vec![
+        tx_prefix,                                                      // TX_PREFIX
+        Felt::THREE,                                                    // version
+        txn.sender_address,                                             // address
+        poseidon_hash_many(get_resource_bounds_array(txn)?.as_slice()), /* h(tip, resource_bounds_for_fee) */
+        poseidon_hash_many(&txn.paymaster_data),                        // h(paymaster_data)
+        chain_id,                                                       // chain_id
+        txn.nonce,                                                      // nonce
+        get_data_availability_modes_field_element(txn), /* nonce_data_availability ||
+                                                         * fee_data_availability_mode */
+    ];
+
+    Ok(array)
+}
+
+fn get_data_availability_mode_value_as_u64(data_availability_mode: DaMode) -> u64 {
+    match data_availability_mode {
+        DaMode::L1 => 0,
+        DaMode::L2 => 1,
+    }
+}
+
+/// Returns Felt that encodes the data availability modes of the transaction
+fn get_data_availability_modes_field_element(txn: &InvokeTxnV3<Felt>) -> Felt {
+    let da_mode = get_data_availability_mode_value_as_u64(txn.nonce_data_availability_mode.clone())
+        << DATA_AVAILABILITY_MODE_BITS;
+    let da_mode =
+        da_mode + get_data_availability_mode_value_as_u64(txn.fee_data_availability_mode.clone());
+    Felt::from(da_mode)
 }
