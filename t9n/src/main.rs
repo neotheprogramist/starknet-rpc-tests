bcbc95806417ba7a1072aa944e50c7bbfde6cda0
Piotr Stec <piotr.stec2g@gmail.com>
2024-08-29 08:32:53 +0200

    args parsing

--- t9n/src/main.rs
+++ t9n/src/main.rs
@@ -1,10 +1,22 @@
 pub mod txn_validation;
 use txn_validation::validate::validate_txn_json;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long)]
+    file_path: String,
+
+    #[arg(short, long)]
+    public_key: String,
+
+    #[arg(short, long)]
+    chain_id: String,
+}
 
 fn main() {
-    let public_key = "0x39d9e6ce352ad4530a0ef5d5a18fd3303c3606a7fa6ac5b620020ad681cc33b";
-    let chain_id = "0x534e5f5345504f4c4941";
-    match validate_txn_json("t9n/testdata/invoke/invoke_txn_v3_rpc_test.json", &public_key, &chain_id) {
+    let args = Args::parse();
+    match validate_txn_json(&args.file_path, &args.public_key, &args.chain_id) {
         Ok(_) => println!("JSON is valid"),
         Err(e) => println!("Validation error: {}", e),
     }
