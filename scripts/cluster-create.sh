1623c3f6263bb80fdcbb69b06358054df96903b8
Uacias <uaciasdev@gmail.com>
2024-09-17 17:24:06 +0200

    Working version with 2 pvc.

--- scripts/cluster-create.sh
+++ scripts/cluster-create.sh
@@ -2,9 +2,17 @@
 
 set -euo pipefail
 
-# Create the cluster
+# Create the cluster using kind
 kind create cluster --config kind/kind.yaml
 
 # Ensure the default service account exists
 kubectl create serviceaccount default || true
 kubectl create clusterrolebinding default-admin --clusterrole=cluster-admin --serviceaccount=default:default || true
+
+# # Tworzenie katalogów wewnątrz kind-control-plane
+# docker exec kind-control-plane bash -c "mkdir -p /mnt/data/pathfinder"
+# docker exec kind-control-plane bash -c "mkdir -p /mnt/data/pathfinder-2"
+
+# # Ustawienie właściciela katalogów
+# docker exec kind-control-plane bash -c "chown -R 1000:1000 /mnt/data/pathfinder"
+# docker exec kind-control-plane bash -c "chown -R 1000:1000 /mnt/data/pathfinder-2"
