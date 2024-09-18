3502e2080e6d658adc89bb810a9cc7e8c6612665
Piotr Stec <piotr.stec2g@gmail.com>
2024-09-18 15:17:04 +0200

    fmt

--- b11r/src/block_build/block.rs
+++ b11r/src/block_build/block.rs
@@ -1,61 +1,33 @@
-use pathfinder_types::types::block_hash::{
-    calculate_event_commitment, calculate_receipt_commitment, calculate_transaction_commitment,
-    compute_final_hash,
+use pathfinder_types::{
+    starknet::state_diff::StateDiff,
+    types::{
+        block::{Block, BlockHeader, BlockHeaderData},
+        block_builder_input::{B11rInput, BlockHash},
+        block_hash::{
+            calculate_event_commitment, calculate_receipt_commitment,
+            calculate_transaction_commitment, compute_final_hash,
+        },
+        event::{extract_emmited_events, get_events_count, Event},
+        receipt::convert_receipts,
+        reply::{state_update::StateDiff as GatewayStateDiff, StateUpdate as GatewayStateUpdate},
+        state_update::{state_diff_commitment::compute, StateUpdate},
+    },
 };
 
-use pathfinder_types::starknet::state_diff::BlockStateDiff;
-use pathfinder_types::types::block::{Block, BlockHeader, BlockHeaderData};
-use pathfinder_types::types::event::{extract_emmited_events, get_events_count, Event};
-use pathfinder_types::types::receipt::convert_receipts;
-use pathfinder_types::types::reply::state_update::StateDiff as GatewayStateDiff;
-use pathfinder_types::types::reply::StateUpdate as GatewayStateUpdate;
-use pathfinder_types::types::state_update::{state_diff_commitment::compute, StateUpdate};
 use starknet_devnet_types::rpc::transaction_receipt::TransactionReceipt;
+
 use starknet_types_core::felt::Felt;
+
 use starknet_types_rpc::v0_7_1::starknet_api_openrpc::TxnWithHash;
-use std::fs::File;
-use std::io::Read;
-use std::path::PathBuf;
 
-pub fn build_block_tx_hashes(
-    block_header_path: PathBuf,
-    transactions_path: PathBuf,
-    receipt_path: PathBuf,
-    state_diff_path: PathBuf,
-) -> Result<Block, Box<dyn std::error::Error>> {
-    let mut header_file = File::open(block_header_path).expect("Unable to open file");
+use super::errors::Error;
 
-    let mut header = String::new();
-    header_file
-        .read_to_string(&mut header)
-        .expect("Unable to read file");
+pub fn build_block_tx_hashes(b11r_input: B11rInput) -> Result<Block, Error> {
+    let block_header: BlockHeader = b11r_input.blocks.get_last_block_header().unwrap();
 
-    let mut transactions_file = File::open(transactions_path).expect("Unable to open file");
+    let transactions: Vec<TxnWithHash<Felt>> = b11r_input.transactions.to_txn_with_hash_vec();
 
-    let mut txns = String::new();
-    transactions_file
-        .read_to_string(&mut txns)
-        .expect("Unable to read file");
-
-    let mut receipts_file = File::open(receipt_path).expect("Unable to open file");
-
-    let mut receipts = String::new();
-    receipts_file
-        .read_to_string(&mut receipts)
-        .expect("Unable to read file");
-
-    let mut state_diff_file = File::open(state_diff_path).expect("Unable to open file");
-
-    let mut state_diff = String::new();
-    state_diff_file
-        .read_to_string(&mut state_diff)
-        .expect("Unable to read file");
-
-    let block_header: BlockHeader = serde_json::from_str(&header)?;
-
-    let transactions: Vec<TxnWithHash<Felt>> = serde_json::from_str(&txns)?;
-
-    let transaction_receipts: Vec<TransactionReceipt> = serde_json::from_str(&receipts)?;
+    let transaction_receipts: Vec<TransactionReceipt> = b11r_input.transaction_receipts;
 
     let events = extract_emmited_events(transaction_receipts.clone());
 
@@ -65,13 +37,12 @@ pub fn build_block_tx_hashes(
         .map(|emitted_event| (emitted_event.transaction_hash, emitted_event.events))
         .collect();
 
-    let block_state_diff: BlockStateDiff = serde_json::from_str(&state_diff)?;
-    let block_hash = block_state_diff.hash_to_state_diff.block_hash;
-    let state_diff = block_state_diff.hash_to_state_diff.state_diff;
-    let state_diff_gateway: GatewayStateDiff = state_diff.into();
+    let (block_hash, block_state_diff): (BlockHash, StateDiff) =
+        b11r_input.blocks.get_last_state_diff_with_hash().unwrap();
+
+    let state_diff_gateway: GatewayStateDiff = block_state_diff.into();
 
     let state_update_gateway = GatewayStateUpdate::new(block_hash, state_diff_gateway);
-
     let state_update_common: StateUpdate = state_update_gateway.into();
 
     let state_diff_commitment = compute(
