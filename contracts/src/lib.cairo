f03f6d4cb1173ca90d2501c391bc41b39d8ef9af
Uacias <filgum0326@gmail.com>
2024-11-11 10:59:32 +0100

    Add test_get_txn_by_block_id_and_index_declare_v3.

--- contracts/src/lib.cairo
+++ contracts/src/lib.cairo
@@ -2,6 +2,7 @@ mod sample_contract_1;
 mod sample_contract_2;
 mod sample_contract_3;
 mod sample_contract_4;
+mod sample_contract_5;
 
 /// Paymaster implementation.
 mod paymaster {
--- openrpc-testgen/src/suite_openrpc/mod.rs
+++ openrpc-testgen/src/suite_openrpc/mod.rs
@@ -48,6 +48,8 @@ pub mod test_get_transaction_by_hash;
 pub mod test_get_transaction_by_hash_non_existent;
 pub mod test_get_txn_by_block_id_and_index;
 pub mod test_get_txn_by_block_id_and_index_declare_v2;
+pub mod test_get_txn_by_block_id_and_index_declare_v3;
+
 #[derive(Clone, Debug)]
 pub struct TestSuiteOpenRpc {
     pub random_paymaster_account: RandomSingleOwnerAccount,
