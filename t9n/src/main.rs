aa1370fe2180dfe927052bbafbd7a97734f46551
Piotr Stec <piotr.stec2g@gmail.com>
2024-11-19 12:54:56 +0100

    add optional public key

--- t9n/src/main.rs
+++ t9n/src/main.rs
@@ -7,7 +7,7 @@ use txn_validation::validate::validate_txn_json;
 
 fn main() {
     let args = Args::parse();
-    match validate_txn_json(&args.file_path, &args.public_key, &args.chain_id) {
+    match validate_txn_json(&args.file_path, args.public_key.as_deref(), &args.chain_id) {
         Ok(json_result) => {
             println!("Validation successful: {}", json_result);
         }
