9493921cdc1e1ed3d63cd9b676c57e8d934f0299
Piotr Stec <piotr.stec2g@gmail.com>
2024-11-28 12:07:00 +0100

    test declare deploy

---
+++ openrpc-testgen/src/suite_katana/test_declare_and_deploy_contract.rs
@@ -0,0 +1,109 @@
+use crate::{
+    assert_eq_result, assert_result,
+    utils::v7::{
+        accounts::{
+            account::{Account, ConnectedAccount},
+            call::Call,
+            deployment::helpers::get_contract_address,
+        },
+        endpoints::{
+            declare_contract::prepare_contract_declaration_params,
+            errors::OpenRpcTestGenError,
+            utils::{get_selector_from_name, wait_for_sent_transaction},
+        },
+        providers::provider::Provider,
+    },
+    RandomizableAccountsTrait, RunnableTrait,
+};
+use starknet_types_core::felt::Felt;
+use starknet_types_rpc::{BlockId, BlockTag};
+
+use std::{path::PathBuf, sync::Arc};
+
+#[derive(Clone, Debug)]
+pub struct TestCase {}
+
+impl RunnableTrait for TestCase {
+    type Input = super::TestSuiteKatana;
+    async fn run(test_input: &Self::Input) -> Result<Self, OpenRpcTestGenError> {
+        let path: PathBuf =
+            PathBuf::from("openrpc-testgen/src/suite_katana/test_data/cairo1_contract.json");
+
+        let (flattened_sierra_class, compiled_class_hash) =
+            prepare_contract_declaration_params(&path)?;
+
+        let provider = test_input
+            .random_paymaster_account
+            .random_accounts()?
+            .provider()
+            .clone();
+
+        let declare_res = test_input
+            .random_paymaster_account
+            .declare_v2(
+                Arc::new(flattened_sierra_class.clone()),
+                compiled_class_hash,
+            )
+            .send()
+            .await?;
+
+        let (transaction_hash, class_hash) = (declare_res.transaction_hash, declare_res.class_hash);
+
+        wait_for_sent_transaction(
+            transaction_hash,
+            &test_input.random_paymaster_account.random_accounts()?,
+        )
+        .await?;
+
+        let get_class_ok = provider
+            .clone()
+            .get_class(BlockId::Tag(BlockTag::Pending), class_hash)
+            .await
+            .is_ok();
+
+        assert_result!(get_class_ok);
+
+        let ctor_args = vec![Felt::ONE, Felt::TWO];
+        let calldata = [
+            vec![
+                declare_res.class_hash,      // class hash
+                Felt::ZERO,                  // salt
+                Felt::ZERO,                  // unique
+                Felt::from(ctor_args.len()), // constructor calldata len
+            ],
+            ctor_args.clone(),
+        ]
+        .concat();
+
+        // pre-compute the contract address of the would-be deployed contract
+        let address =
+            get_contract_address(Felt::ZERO, declare_res.class_hash, &ctor_args, Felt::ZERO);
+
+        let res = test_input
+            .random_paymaster_account
+            .execute_v1(vec![Call {
+                calldata,
+                to: Felt::from_hex_unchecked(
+                    "0x041a78e741e5af2fec34b695679bc6891742439f7afb8484ecd7766661ad02bf", //DEFAULT_UDC_ADDRESS
+                ),
+                selector: get_selector_from_name("deployContract")?,
+            }])
+            .send()
+            .await?;
+
+        wait_for_sent_transaction(
+            res.transaction_hash,
+            &test_input.random_paymaster_account.random_accounts()?,
+        )
+        .await?;
+
+        // make sure the contract is deployed
+        let res = provider
+            .get_class_hash_at(BlockId::Tag(BlockTag::Pending), address)
+            .await?;
+
+        assert_eq_result!(res, class_hash);
+
+        Ok(Self {})
+    }
+}
