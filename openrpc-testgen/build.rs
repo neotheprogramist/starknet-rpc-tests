e3e5d8e75a95a560a9aeb8be914d7f4a3ee6c4d4
Piotr Stec <piotr.stec2g@gmail.com>
2024-11-28 12:09:18 +0100

    testgen build rs updates

--- openrpc-testgen/build.rs
+++ openrpc-testgen/build.rs
@@ -99,7 +99,7 @@ fn process_module_directory(
 
     writeln!(
         file,
-        "    async fn run(input: &Self::Input) -> Result<Self, crate::utils::v7::endpoints::errors::RpcError> {{"
+        "    async fn run(input: &Self::Input) -> Result<Self, crate::utils::v7::endpoints::errors::OpenRpcTestGenError> {{"
     )
     .unwrap();
 
@@ -127,22 +127,17 @@ fn process_module_directory(
         writeln!(
             file,
             "        match {}::{}::TestCase::run(&data).await {{
-                Ok(test_case) => match test_case.result {{
-                    Ok(_) => tracing::info!(
-                        \"{{}}\", 
-                        \"✓ Test case src/{} completed successfully.\".green()
-                    ),
-                    Err(e) => tracing::error!(
-                        \"{{}}\", 
-                        format!(\"✗ Test case src/{} failed with error: {{:?}}\", e).red()
-                    ),
-                }},
+                Ok(_) => tracing::info!(
+                    \"{{}}\", 
+                    \"✓ Test case src/{} completed successfully.\".green()
+                ),
+
                 Err(e) => tracing::error!(
                     \"{{}}\", 
                     format!(\"✗ Test case src/{} failed with runtime error: {{:?}}\", e).red()
                 ),
-}}",
-            module_prefix, test_name, test_name, test_name, test_name
+        }}",
+            module_prefix, test_name, test_name, test_name
         )
         .unwrap();
     }
@@ -173,7 +168,7 @@ fn partition_modules(mod_file_path: &Path) -> (Vec<String>, Vec<String>) {
     let mut nested_suites = Vec::new();
 
     for line in content.lines() {
-        if line.trim_start().starts_with("pub mod ") {
+        if line.trim_start().starts_with("pub mod test_") {
             let parts: Vec<&str> = line.split_whitespace().collect();
             if parts.len() >= 3 {
                 let mod_name = parts[2].trim_end_matches(';').to_string();
